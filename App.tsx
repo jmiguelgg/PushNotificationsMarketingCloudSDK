78d7294b8e70949d87dfd934e52ac62ac19cc82e
jmiguelgg1 <jose.gutierrezg@agilethought.com>
2023-02-08 14:05:41 -0600

    feat: implemente permissions on android and some configuration on mainaplication.java

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import MCReactModule from 'react-native-marketingcloudsdk';
 
 const App = (): JSX.Element => {
+  const updateRegistrationData = async () => {
+    MCReactModule.setContactKey('jmiguel_guevara@outlook.com');
+    const contactKey = await MCReactModule.getContactKey();
+    const attrs = await MCReactModule.getAttributes();
+    const tags = await MCReactModule.getTags();
+    const systemToken = await MCReactModule.getSystemToken();
+    const isPushEnabled = await MCReactModule.isPushEnabled();
+    console.log('[APP]: contactKey: ', contactKey);
+    console.log('[APP]: attrs: ', attrs);
+    console.log('[APP]: tags: ', tags);
+    console.log('[APP]: systemToken: ', systemToken);
+    console.log('[APP]: isPushEnabled: ', isPushEnabled);
+  };
+
+  useEffect(() => {
+    updateRegistrationData().catch(console.error);
+  }, []);
+
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
--- android/app/src/main/java/com/testpush/MainApplication.java
+++ android/app/src/main/java/com/testpush/MainApplication.java
@@ -1,6 +1,8 @@
 package com.testpush;
 
 import android.app.Application;
+import android.util.Log;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -8,6 +10,10 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.salesforce.marketingcloud.MarketingCloudConfig;
+import com.salesforce.marketingcloud.MarketingCloudSdk;
+import com.salesforce.marketingcloud.notifications.NotificationCustomizationOptions;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -53,6 +59,19 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    MarketingCloudSdk.init(this,
+            MarketingCloudConfig.builder()
+                    .setApplicationId("1ab557be-899f-4e6b-b9f2-6b440a82c1a5")
+                    .setAccessToken("6lWOsJKQEb7MZgkYc3lUwzdR")
+                    .setSenderId("AAAAaqYkHrc:APA91bHHkg-vlDh1dal0XgLZb2fP_W4vpEfj3rfkB2pto6nyEodZuPUVcT0LmFVyocIJz3oLumDXaLpsv0iQ88cblhsCmMk82bJYE-8baMfN_gT-BvPTya8YvQCROsppu-624hBebvOC")
+                    .setMarketingCloudServerUrl("https://mcmqczd118jdz8xqdfsbxdwh96f0.device.marketingcloudapis.com/")
+                    .setNotificationCustomizationOptions(NotificationCustomizationOptions.create(R.drawable.common_google_signin_btn_icon_disabled))
+                    .setAnalyticsEnabled(true)
+                    .setGeofencingEnabled(true)
+                    .setInboxEnabled(true)
+                    .setDelayRegistrationUntilContactKeyIsSet(true)
+                    .build(this),
+            initializationStatus -> Log.e("[MainApplication]: INIT", initializationStatus.toString()));
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
